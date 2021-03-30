6ce71adb9054d992972a910e49c26d46c970648c
El Famoso <40206612+ElFamosso@users.noreply.github.com>
2021-03-30 17:01:22 +0200

    Add files via upload

--- rsa.py
+++ rsa.py
@@ -24,6 +24,7 @@ def generecle(tailleenbit=1024):
     print("e = ",e)
     print("phiN = ",phiN)
     print("d = ",d)
+    print("n = ",n)
     return e,d,n
     
 def inversemodule(e,phiN):
@@ -57,6 +58,7 @@ def coprem(p,q):
 
 
 def estpremier(nombre):
+    
     if nombre < 2:
         return False
     petitpremiers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
@@ -95,12 +97,44 @@ def cryptage2(msg,e,n):
 
 def cryptage3(msg,e,n):
     l = [x for x in bytes(msg, encoding="utf8")]
-    listecryptage = []
-    for i in range(n//10):
+    listecryptage=[]
+    for i in range(len(l)//3):
+        listecryptage.append(l[i*3:(i+1)*3])
+    listecryptage.append(l[len(l)-len(l)%3:len(l)])
+    
+    
+    crypte = []
+    listecrypteRSA = []
+    
+    for item in listecryptage:
+        somme = 0
+        for i in range(0,len(item)):
+            somme += (item[i])*2**(8*(2-i))
+        crypte.append(somme)
+    for item in crypte:
+        listecrypteRSA.append(pow(item,e,n))
+        
+    print(listecryptage)
+    print(crypte)
+    print(listecrypteRSA)
+    return listecrypteRSA
+
         
     
-    
-    return listecryptage
+def decryptage3(crypte,d,n):
+    decrypte = []
+    msg = ''
+    for item in crypte:
+        decrypte.append(pow(item,d,n))
+    for item in decrypte:
+        l = []
+        for i in range(3):
+            element = item//(2**((2-i)*8))
+            l.append(element)
+            item = item%(2**((2-i)*8))
+        print(l)
+        msg += str(bytes(l),encoding="utf-8")
+        
 
 def decryptage(crypte,d,n):
     global a, b, c
@@ -131,11 +165,10 @@ def grandnombrepremier(tailleenbit=1024): #mettre une taille de 1024 bit si pas 
 if __name__ == '__main__':
     tailleenbit = int(input("Taille de votre clé : "))
     e,d,n = generecle(tailleenbit)
-    msg = "bonjour"
+    msg = "bonjour je suis malo"
 
-    crypt = cryptage2(msg,e, n)
+    crypt = cryptage3(msg,e, n)
     print(crypt)
-    #○dec = decryptage2(crypt,d, n)
-    #print(crypt)
-    #print(dec)
+    dec = decryptage3(crypt,d, n)
+    print(dec)
 
